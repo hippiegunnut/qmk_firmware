51f65fc82800df802ab4f047fb5fb386100ce5e8
Molly Fletcher <molly@imp.molanie.org>
2020-07-22 11:07:57 +0100

    Revert "tidy up and add printscreen on raise/lower <Funk>"
    This reverts commit 7a5c9579a3265771a67b2388c0d7d03bff83adf5.

--- keyboards/planck/rev5/rules.mk
+++ keyboards/planck/rev5/rules.mk
@@ -16,11 +16,11 @@ OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
-EXTRAKEY_ENABLE = no       # Audio control and System control(+450)
-CONSOLE_ENABLE = no         # Console for debug(+400)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+CONSOLE_ENABLE = yes         # Console for debug(+400)
 COMMAND_ENABLE = no        # Commands for debug and configuration
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = yes           # Audio output on port C6
 UNICODE_ENABLE = no         # Unicode
